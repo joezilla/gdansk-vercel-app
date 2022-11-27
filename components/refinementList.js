8ff698d23c0fdfd0b665f10979929b590645d06d
Joe Toppe <jochen@jtoee.com>
2022-11-27 17:32:33 -0500

    search mostly working

---
+++ components/refinementList.js
@@ -0,0 +1,28 @@
+
+import React from 'react';
+
+import { connectRefinementList } from "react-instantsearch-dom";
+
+
+function RefinementRenderer(renderOptions) {
+    const { items, refine, createURL } = renderOptions;
+    return (
+        <>
+            {items.map((item) => (
+                <div key={item.label} className="flex flex-col space-y-1">
+                    <label className="pr-1">
+                        <input
+                            type="checkbox"
+                            value={item.value}
+                            checked={item.isRefined}
+                            onChange={(event) => refine(item.value)}
+                        />
+                        <span className="pl-1">{item.label}</span>
+                    </label>
+                </div>
+            ))}
+        </>
+    );
+}
+
+export default connectRefinementList(RefinementRenderer);
