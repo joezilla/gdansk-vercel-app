cb513aeb856068734a0ef0d08c52bf15f8bd4349
Joe Toppe <jochen@jtoee.com>
2022-12-18 17:33:45 -0500

    fix

--- components/layout/layout.tsx
+++ components/layout/layout.tsx
@@ -14,21 +14,12 @@ export function Layout(props: LayoutProps) {
   return (
     <>
       <Meta />
-      <Script id="google-tag-manager" strategy="afterInteractive">
-        {`
-        <!-- Google Tag Manager (noscript) -->
-        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-W6NVS67"
-        height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-        <!-- End Google Tag Manager (noscript) -->
-      `}
-      </Script>
       <div className="dark:bg-mybg-dark dark:text-mytxt-dark">
         <Navi navigationPosts={props.navigationPosts}/>
         <main className="py-0">{props.children}</main>
       </div>
       <Footer />
       <Script src="/scripts/darkmode.js" strategy="lazyOnload" />
-
     </>
   )
 }
