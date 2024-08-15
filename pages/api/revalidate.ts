394b8d136f6879564b05579b61753fb31401a8f3
Mr Eppot <jochen.toppe@gmail.com>
2024-08-15 17:15:44 -0400

    testing in prod

--- pages/api/revalidate.ts
+++ pages/api/revalidate.ts
@@ -9,6 +9,7 @@ import { log } from 'next-axiom'
 import { revalidatePath } from 'next/cache';
 import { revalidateTag } from "next/cache";
 import { NextResponse } from "next/server";
+import { ObjectCache } from '../../lib/objectcache';
 
 type ServiceResponse = {
     result: any
@@ -22,12 +23,16 @@ export default async function handler(
 ) {
     const API_KEY = process.env.API_KEY;
     const SECRET = req?.headers?.apisecret ?? "";
+    const objectCache = new ObjectCache();
 
     try {
         if (SECRET === API_KEY) {
             // Process the POST request
             try {
                 const fromHook = req.body as any;
+
+                log.debug(`request body: ${ JSON.stringify(req.body)}`);
+               
 
                 var slug = fromHook.fields.slug['en-US'];
                 var id = fromHook.sys.id;
@@ -48,6 +53,9 @@ export default async function handler(
                 } else {
                     log.error(`Cannot handle type ${type}`);
                 }
+
+                // todo - make this less nuclear
+                objectCache.clearCache();
 
                 res.status(200).json({ result: "ok" })
 
